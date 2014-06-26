c2576969b1f8b7c931d991b0ca91b7873aca1d70
Robert Baykov <baykovr@cs.tamu.edu>
2014-06-26 14:53:28 -0500

    minor tweaks, still working on gui

--- GAVPI/GAVPI/frmActionSequence.Designer.cs
+++ GAVPI/GAVPI/frmActionSequence.Designer.cs
@@ -28,44 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lstActSeqActions = new System.Windows.Forms.ListView();
+            this.txtActionSequenceName = new System.Windows.Forms.TextBox();
             this.lblActionSequenceName = new System.Windows.Forms.Label();
+            this.cboxActSeqAction = new System.Windows.Forms.ComboBox();
+            this.lblAddAction = new System.Windows.Forms.Label();
+            this.btnActSeqSave = new System.Windows.Forms.Button();
+            this.btnActSeqCancel = new System.Windows.Forms.Button();
+            this.btnActSeqAdd = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // listView1
+            // lstActSeqActions
             // 
-            this.listView1.GridLines = true;
-            this.listView1.Location = new System.Drawing.Point(12, 52);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(632, 300);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.lstActSeqActions.GridLines = true;
+            this.lstActSeqActions.Location = new System.Drawing.Point(12, 98);
+            this.lstActSeqActions.Name = "lstActSeqActions";
+            this.lstActSeqActions.Size = new System.Drawing.Size(434, 365);
+            this.lstActSeqActions.TabIndex = 0;
+            this.lstActSeqActions.UseCompatibleStateImageBehavior = false;
             // 
-            // textBox1
+            // txtActionSequenceName
             // 
-            this.textBox1.Location = new System.Drawing.Point(138, 23);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(506, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtActionSequenceName.Location = new System.Drawing.Point(138, 9);
+            this.txtActionSequenceName.Name = "txtActionSequenceName";
+            this.txtActionSequenceName.Size = new System.Drawing.Size(265, 20);
+            this.txtActionSequenceName.TabIndex = 1;
             // 
             // lblActionSequenceName
             // 
             this.lblActionSequenceName.AutoSize = true;
-            this.lblActionSequenceName.Location = new System.Drawing.Point(12, 26);
+            this.lblActionSequenceName.Location = new System.Drawing.Point(12, 9);
             this.lblActionSequenceName.Name = "lblActionSequenceName";
             this.lblActionSequenceName.Size = new System.Drawing.Size(120, 13);
             this.lblActionSequenceName.TabIndex = 2;
             this.lblActionSequenceName.Text = "Action Sequence Name";
             // 
+            // cboxActSeqAction
+            // 
+            this.cboxActSeqAction.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboxActSeqAction.FormattingEnabled = true;
+            this.cboxActSeqAction.Location = new System.Drawing.Point(55, 62);
+            this.cboxActSeqAction.Name = "cboxActSeqAction";
+            this.cboxActSeqAction.Size = new System.Drawing.Size(227, 21);
+            this.cboxActSeqAction.TabIndex = 3;
+            // 
+            // lblAddAction
+            // 
+            this.lblAddAction.AutoSize = true;
+            this.lblAddAction.Location = new System.Drawing.Point(12, 65);
+            this.lblAddAction.Name = "lblAddAction";
+            this.lblAddAction.Size = new System.Drawing.Size(37, 13);
+            this.lblAddAction.TabIndex = 5;
+            this.lblAddAction.Text = "Action";
+            // 
+            // btnActSeqSave
+            // 
+            this.btnActSeqSave.Location = new System.Drawing.Point(290, 469);
+            this.btnActSeqSave.Name = "btnActSeqSave";
+            this.btnActSeqSave.Size = new System.Drawing.Size(75, 23);
+            this.btnActSeqSave.TabIndex = 6;
+            this.btnActSeqSave.Text = "Save";
+            this.btnActSeqSave.UseVisualStyleBackColor = true;
+            // 
+            // btnActSeqCancel
+            // 
+            this.btnActSeqCancel.Location = new System.Drawing.Point(371, 469);
+            this.btnActSeqCancel.Name = "btnActSeqCancel";
+            this.btnActSeqCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnActSeqCancel.TabIndex = 7;
+            this.btnActSeqCancel.Text = "Cancel";
+            this.btnActSeqCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnActSeqAdd
+            // 
+            this.btnActSeqAdd.Location = new System.Drawing.Point(290, 62);
+            this.btnActSeqAdd.Name = "btnActSeqAdd";
+            this.btnActSeqAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnActSeqAdd.TabIndex = 8;
+            this.btnActSeqAdd.Text = "Add";
+            this.btnActSeqAdd.UseVisualStyleBackColor = true;
+            // 
             // frmActionSequence
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(656, 504);
+            this.ClientSize = new System.Drawing.Size(463, 504);
+            this.Controls.Add(this.btnActSeqAdd);
+            this.Controls.Add(this.btnActSeqCancel);
+            this.Controls.Add(this.btnActSeqSave);
+            this.Controls.Add(this.lblAddAction);
+            this.Controls.Add(this.cboxActSeqAction);
             this.Controls.Add(this.lblActionSequenceName);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.txtActionSequenceName);
+            this.Controls.Add(this.lstActSeqActions);
             this.Name = "frmActionSequence";
             this.Text = "Action Sequence";
             this.ResumeLayout(false);
@@ -75,8 +130,13 @@
 
         #endregion
 
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView lstActSeqActions;
+        private System.Windows.Forms.TextBox txtActionSequenceName;
         private System.Windows.Forms.Label lblActionSequenceName;
+        private System.Windows.Forms.ComboBox cboxActSeqAction;
+        private System.Windows.Forms.Label lblAddAction;
+        private System.Windows.Forms.Button btnActSeqSave;
+        private System.Windows.Forms.Button btnActSeqCancel;
+        private System.Windows.Forms.Button btnActSeqAdd;
     }
 }
--- GAVPI/GAVPI/frmProfile.Designer.cs
+++ GAVPI/GAVPI/frmProfile.Designer.cs
@@ -28,20 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuProfile = new System.Windows.Forms.MenuStrip();
+            this.splitProfile = new System.Windows.Forms.SplitContainer();
+            this.dgTriggers = new System.Windows.Forms.DataGridView();
+            this.dgActionSeqeunces = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitProfile)).BeginInit();
+            this.splitProfile.Panel1.SuspendLayout();
+            this.splitProfile.Panel2.SuspendLayout();
+            this.splitProfile.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgTriggers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgActionSeqeunces)).BeginInit();
             this.SuspendLayout();
+            // 
+            // menuProfile
+            // 
+            this.menuProfile.Location = new System.Drawing.Point(0, 0);
+            this.menuProfile.Name = "menuProfile";
+            this.menuProfile.Size = new System.Drawing.Size(950, 24);
+            this.menuProfile.TabIndex = 0;
+            this.menuProfile.Text = "menuStrip1";
+            // 
+            // splitProfile
+            // 
+            this.splitProfile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitProfile.Location = new System.Drawing.Point(0, 24);
+            this.splitProfile.Name = "splitProfile";
+            // 
+            // splitProfile.Panel1
+            // 
+            this.splitProfile.Panel1.Controls.Add(this.dgTriggers);
+            // 
+            // splitProfile.Panel2
+            // 
+            this.splitProfile.Panel2.Controls.Add(this.dgActionSeqeunces);
+            this.splitProfile.Size = new System.Drawing.Size(950, 525);
+            this.splitProfile.SplitterDistance = 350;
+            this.splitProfile.TabIndex = 1;
+            // 
+            // dgTriggers
+            // 
+            this.dgTriggers.AllowUserToAddRows = false;
+            this.dgTriggers.AllowUserToDeleteRows = false;
+            this.dgTriggers.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgTriggers.BackgroundColor = System.Drawing.Color.White;
+            this.dgTriggers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgTriggers.Location = new System.Drawing.Point(3, 23);
+            this.dgTriggers.Name = "dgTriggers";
+            this.dgTriggers.ReadOnly = true;
+            this.dgTriggers.Size = new System.Drawing.Size(344, 499);
+            this.dgTriggers.TabIndex = 0;
+            // 
+            // dgActionSeqeunces
+            // 
+            this.dgActionSeqeunces.AllowUserToAddRows = false;
+            this.dgActionSeqeunces.AllowUserToDeleteRows = false;
+            this.dgActionSeqeunces.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgActionSeqeunces.BackgroundColor = System.Drawing.Color.White;
+            this.dgActionSeqeunces.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgActionSeqeunces.GridColor = System.Drawing.Color.White;
+            this.dgActionSeqeunces.Location = new System.Drawing.Point(3, 23);
+            this.dgActionSeqeunces.Name = "dgActionSeqeunces";
+            this.dgActionSeqeunces.ReadOnly = true;
+            this.dgActionSeqeunces.Size = new System.Drawing.Size(590, 499);
+            this.dgActionSeqeunces.TabIndex = 0;
             // 
             // frmProfile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(950, 549);
+            this.Controls.Add(this.splitProfile);
+            this.Controls.Add(this.menuProfile);
+            this.MainMenuStrip = this.menuProfile;
             this.Name = "frmProfile";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Profile";
+            this.splitProfile.Panel1.ResumeLayout(false);
+            this.splitProfile.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitProfile)).EndInit();
+            this.splitProfile.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgTriggers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgActionSeqeunces)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuProfile;
+        private System.Windows.Forms.SplitContainer splitProfile;
+        private System.Windows.Forms.DataGridView dgTriggers;
+        private System.Windows.Forms.DataGridView dgActionSeqeunces;
     }
 }
