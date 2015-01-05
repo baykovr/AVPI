ad4ad7756b600f99fd483d58d9af252ae113c803
Robert Baykov <baykovr@cs.tamu.edu>
2015-01-05 14:50:47 -0600

    Data Add/Edit Form

--- GAVPI/GAVPI/frm_AddEdit_Data.cs
+++ GAVPI/GAVPI/frm_AddEdit_Data.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GAVPI
 {
-    public partial class frm_AddEdit_VI_Data : Form
+    public partial class frm_AddEdit_Data : Form
     {
-        public frm_AddEdit_VI_Data()
+        public frm_AddEdit_Data()
         {
             InitializeComponent();
         }
