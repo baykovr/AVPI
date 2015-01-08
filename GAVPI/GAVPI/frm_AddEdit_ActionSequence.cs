0ac54707ff3c2bcca469a1ec1224c879cb98c99b
Robert Baykov <baykovr@cs.tamu.edu>
2015-01-08 10:53:08 -0600

    Action Sequence form complete (mostly tested, but needs more)

--- GAVPI/GAVPI/frm_AddEdit_ActionSequence.cs
+++ GAVPI/GAVPI/frm_AddEdit_ActionSequence.cs
@@ -11,13 +11,17 @@ using System.Windows.Forms;
 
 namespace GAVPI
 {   
+    // TODO :
+    // When editing the name of a sequence
+    // since we remove and then add()
+    // the new name will be inserted at the end of the list..
+    // changing the order in profile form.
+
     public partial class frm_AddEdit_ActionSequence : Form
     {
         public bool ActionSequenceEdited;
 
         private VI_Action_Sequence sequence_to_edit;
-
-        private List<Action> actions_to_edit;
 
         public static List<string> Action_Groups = new List<string>(
             new string[] { 
@@ -46,10 +50,7 @@ namespace GAVPI
         private void populate_fields()
         {
             // Fill combo-box with possible action types.
-            cbActionType.DataSource = Action_Groups;
-
-            // and refresh it immediatly
-            refresh_action_value_choices();
+            cbActionType.DataSource = Action_Groups; //Used to be ActionSequence.Types (now we group them into forms)
             
             // Null the data grid, later we will bind actions to it (as list)
             dgActionSequence.DataSource = null;
@@ -59,7 +60,7 @@ namespace GAVPI
             {
                 txtActionSequenceName.Text = sequence_to_edit.name;
 
-                actions_to_edit = sequence_to_edit.action_sequence.ToList();
+                dgActionSequence.DataSource = sequence_to_edit.action_sequence.ToList();
 
                 txtActionSequenceComment.Text = sequence_to_edit.comment;
             }
@@ -67,10 +68,9 @@ namespace GAVPI
             {
                 this.sequence_to_edit = new VI_Action_Sequence();
 
-                actions_to_edit = new List<Action>();
+                dgActionSequence.DataSource = new List<Action>();
             }
 
-            dgActionSequence.DataSource = actions_to_edit;
         }
         private void refresh_dgActionSequence()
         {
@@ -81,53 +81,13 @@ namespace GAVPI
             }
         }
 
-        private void refresh_action_value_choices()
-        {
-            // DEPRECIATED
-
-            //else if (
-            //    cbActionType.SelectedItem.ToString() == "MouseKeyDown" ||
-            //    cbActionType.SelectedItem.ToString() == "MouseKeyUp" ||
-            //    cbActionType.SelectedItem.ToString() == "MouseKeyPress")
-            //{
-            //    cbActSeqActionValue.DropDownStyle = ComboBoxStyle.DropDownList;
-            //    cbActSeqActionValue.DataSource = Enum.GetValues(typeof(Mouse.MouseKeys)).Cast<Mouse.MouseKeys>();
-            //}
-
-            //else if (cbActionType.SelectedItem.ToString() == "Wait")
-            //{
-            //    cbActSeqActionValue.DropDownStyle = ComboBoxStyle.Simple;
-
-            //}
-
-            //else if (cbActionType.SelectedItem.ToString() == "Speak")
-            //{
-            //    cbActSeqActionValue.DropDownStyle = ComboBoxStyle.Simple;
-            //}
-            //else if (
-            //    cbActionType.SelectedItem.ToString() == "Data Set" ||
-            //    cbActionType.SelectedItem.ToString() == "Data Increment" ||
-            //    cbActionType.SelectedItem.ToString() == "Data Decrement"
-            //    )
-            //{
- 
-            //}
-            //else if (cbActionType.SelectedItem.ToString() == "Data Speak")
-            //{
- 
-            //}
-            //else
-            //{
-            //    MessageBox.Show("WARNING: This action type is not implemented!");
-            //    //throw new NotImplementedException();
-            //}
-        }
         private void btnActSeqCancel_Click(object sender, EventArgs e)
         {
 			this.DialogResult = DialogResult.Cancel;
             this.Close();
         }
 
+        // This is the most important function, handles validation and commiting.
         private void btnActSeqSave_Click(object sender, EventArgs e)
         {
             string aseq_name = txtActionSequenceName.Text.Trim();
@@ -141,134 +101,30 @@ namespace GAVPI
                 return;
             }
             
-
-            // New Sequence
-            if (sequence_to_edit == null)
+            //Check if new name is taken, this requires first removing the sequence (in case no changes)
+            GAVPI.vi_profile.Profile_ActionSequences.Remove(sequence_to_edit);
+                
+            if (GAVPI.vi_profile.isActionSequenceNameTaken(aseq_name))
             {
-
+                // Reinsert without modifications.
+                GAVPI.vi_profile.Profile_ActionSequences.Add(sequence_to_edit);
+                MessageBox.Show("An action sequence with this name already exists.");
+                return;
             }
-            // Editing Existing Sequence
             else
             {
-                // 
-                GAVPI.vi_profile.Profile_ActionSequences.Remove(sequence_to_edit);
+                //Name is not taken, add it in.
 
-                if (GAVPI.vi_profile.isActionSequenceNameTaken(aseq_name))
-                {
-                    // Reinsert without modifications
-                    GAVPI.vi_profile.Profile_ActionSequences.Add(sequence_to_edit);
-                    MessageBox.Show("An action sequence with this name already exists.");
-                    return;
-                }
-                else
-                {
-                    sequence_to_edit.name = aseq_name;
-                    sequence_to_edit.comment = aseq_comment;
-                    
-                    // TODO : Testing
-                    sequence_to_edit.action_sequence = actions_to_edit;
-                }
- 
+                sequence_to_edit.name = aseq_name;
+                sequence_to_edit.comment = aseq_comment;
+
+                GAVPI.vi_profile.Profile_ActionSequences.Add(sequence_to_edit);
+
+                ActionSequenceEdited = true;
+                this.DialogResult = DialogResult.OK;
+                this.Close();
             }
-
-            //foreach (VI_Action_Sequence existing_sequence in GAVPI.vi_profile.Profile_ActionSequences)
-            //{
-            //    if (new_action_sequence_name == existing_sequence.name)
-            //    {
-            //        GAVPI.vi_profile.Profile_ActionSequences.Remove(existing_sequence);
-            //        GAVPI.vi_profile.Profile_ActionSequences.Add(action_sequence);
-            //        // Replace and break
-            //        break;
-            //    }
-            //}
-            //action_sequence.name = new_action_sequence_name;
-            //action_sequence.comment = new_action_sequence_comment;
-
-            //if (edit_mode)
-            //{
-            //    //todo: suboptimal? But is find and replace any better?
-            //    GAVPI.vi_profile.Profile_ActionSequences.Remove(action_sequence);
-            //    GAVPI.vi_profile.Profile_ActionSequences.Add(action_sequence);
-            //}
-            //else
-            //{
-            //    GAVPI.vi_profile.Profile_ActionSequences.Add(action_sequence);
-            //}
-			
-
-            //this.DialogResult = DialogResult.OK;
-			
-            //this.Close();
         }
-
-        private void cbActSeqActionType_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            refresh_action_value_choices();
-        }
-        
-
-        // void btnActSeqAdd_Click(object sender, EventArgs e)
-        //{
-            // TODO
-            // switch on type
-            //frm_AddEdit_PressAction newKeyAction = new frm_AddEdit_PressAction();
-            //if (newKeyAction.ShowDialog() == DialogResult.OK)
-            //{
-            //    ActionSequenceEdited = true;
-               //refresh_dgActionSequence();
-            //} // if()
-
-            
-
-            //--
-            //Type new_action_type = Type.GetType("GAVPI." + cbActSeqActionType.SelectedItem.ToString());
-
-            //object action_instance;
-            //if (new_action_type.ToString() == "GAVPI.Speak")
-            //{
-            //    action_instance = Activator.CreateInstance(new_action_type, GAVPI.vi_profile.synth ,cbActSeqActionValue.Text);
-                
-            //    for (uint i = 0; i < times_to_add; i++)
-            //    {
-            //        action_sequence.Add((Action)action_instance);
-            //    }
-                
-            //    refresh_editactionsequence();
-            //}
-            //else if (new_action_type.ToString() == "GAVPI.Wait")
-            //{
-            //    int test;
-            //    if (Int32.TryParse(cbActSeqActionValue.Text, out test) && test > 0)
-            //    {
-            //        action_instance = Activator.CreateInstance(new_action_type, cbActSeqActionValue.Text);
-            //        for (uint i = 0; i < times_to_add; i++)
-            //        {
-            //            action_sequence.Add((Action)action_instance);
-            //        }
-            //        refresh_editactionsequence();
-            //    }
-            //    else
-            //    {
-            //        MessageBox.Show("Wait must be positive integer in milliseconds.");
-            //        return;
-            //    }
-            //}
-            //else
-            //{
-            //    action_instance = Activator.CreateInstance(new_action_type, cbActSeqActionValue.SelectedItem.ToString());
-            //    for (uint i = 0; i < times_to_add; i++)
-            //    {
-            //        action_sequence.Add((Action)action_instance);
-            //    }
-            //    refresh_editactionsequence();
-            //}
-			
-			//  We've updated the Action Sequence, so allow the user to save their changes...
-			
-            // TODO
-			//this.btnActSeqSave.Enabled = true;
-
-        // }
 
         #region Action Add : Edit : Remove
         // These methods are so because we have right click and button UI event methods
